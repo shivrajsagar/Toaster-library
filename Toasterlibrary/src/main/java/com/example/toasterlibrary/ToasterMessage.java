566d84a3b41dbbd31469b48720fc8c6dbb157c5d
shivrajsagar <shivsagar.sagar294@gmail.com>
2023-06-30 11:22:44 +0530

    add edittext

--- Toasterlibrary/src/main/java/com/example/toasterlibrary/ToasterMessage.java
+++ Toasterlibrary/src/main/java/com/example/toasterlibrary/ToasterMessage.java
@@ -1,10 +1,28 @@
 package com.example.toasterlibrary;
 
+import android.app.Activity;
 import android.content.Context;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.widget.EditText;
 import android.widget.Toast;
 
-public class ToasterMessage {
+import androidx.annotation.Nullable;
+
+public class ToasterMessage extends Activity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.pintab);
+
+
+        EditText et_text = findViewById(R.id.et_text);
+        Toast.makeText(this, ""+ et_text.getText().toString(), Toast.LENGTH_SHORT).show();
+    }
+
     public static void s(Context context, String Message){
         Toast.makeText(context, Message, Toast.LENGTH_SHORT).show();
+
     }
 }
